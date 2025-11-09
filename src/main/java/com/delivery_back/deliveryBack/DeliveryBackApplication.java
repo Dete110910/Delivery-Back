6b0a6435b06871ab9ec554024eb8aedffb5f8c9e
Daniel <alexanderlml1997@gmail.com>
2025-11-08 20:55:25 -0500

    initial commit

---
+++ src/main/java/com/delivery_back/deliveryBack/DeliveryBackApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_back.deliveryBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBackApplication.class, args);
+	}
+
+}
